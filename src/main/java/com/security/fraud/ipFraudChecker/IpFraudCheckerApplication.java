5e53dd8e5272aeef8ebe3ea2b241e44d3ef4a620
davidmarquez98 <davidarielmarquez@outlook.com>
2024-09-28 13:16:09 -0300

    iniciando projecto spring

---
+++ src/main/java/com/security/fraud/ipFraudChecker/IpFraudCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.security.fraud.ipFraudChecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpFraudCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpFraudCheckerApplication.class, args);
+	}
+
+}
