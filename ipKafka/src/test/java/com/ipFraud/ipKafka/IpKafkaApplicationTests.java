300e06325d66ffde458f38b3f26be42d31654aa6
davidmarquez98 <davidarielmarquez@outlook.com>
2024-10-03 15:45:11 -0300

    se ingreso ipKafka

---
+++ ipKafka/src/test/java/com/ipFraud/ipKafka/IpKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipFraud.ipKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
